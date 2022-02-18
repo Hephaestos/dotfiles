317f260d9b868aa3dc46ff25a947ade784937eec
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-18 15:50:46 -0500

    install vim, tmux, and tpm on setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mv dotfiles .dotfiles
+sudo apt install zsh vim tmux
 ln -s .dotfiles/.vimrc .vimrc
 ln -s .dotfiles/.vim .vim
 vim -c "PlugInstall"
 ln -s .dotfiles/.tmux.conf tmux.conf
-sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm .zshrc
