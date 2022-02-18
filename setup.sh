adbec1bb62d4ea9645254b7f90b2712f0cba3a77
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-18 16:53:09 -0500

    fix tmux, add p10k configure and close vim

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@ mv dotfiles .dotfiles
 sudo apt install zsh vim tmux
 ln -s .dotfiles/.vimrc .vimrc
 ln -s .dotfiles/.vim .vim
-vim -c "PlugInstall"
-ln -s .dotfiles/.tmux.conf tmux.conf
+vim -c "PlugInstall | q | q"
+ln -s .dotfiles/.tmux.conf .tmux.conf
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm .zshrc
 ln -s .dotfiles/.zshrc
 source .zshrc
+p10k configure
