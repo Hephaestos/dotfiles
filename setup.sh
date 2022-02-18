5abbd0e4712f1be4ab563088fbfb92b705009be0
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-18 17:12:03 -0500

    fix typo and change oh-my-zsh install

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ ln -s .dotfiles/.vimrc .vimrc
 ln -s .dotfiles/.vim .vim
 vim -c "PlugInstall | q | q"
 ln -s .dotfiles/.tmux.conf .tmux.conf
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm .zshrc
 ln -s .dotfiles/.zshrc
+RUNZSH='no'
+KEEP_ZSHRC='yes'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 source .zshrc
 p10k configure
