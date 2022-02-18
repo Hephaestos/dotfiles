9a3288879d218c9402001d104b08aa6e18063036
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-18 17:37:33 -0500

    add p10k configuration to dotfiles

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ ln -s .dotfiles/.vim .vim
 vim -c "PlugInstall | q | q"
 ln -s .dotfiles/.tmux.conf .tmux.conf
 rm .zshrc
-ln -s .dotfiles/.zshrc
+ln -s .dotfiles/.zshrc .zshrc
+ln -s .dotfiles/.p10k.zsh .p10k.zsh
 KEEP_ZSHRC=yes
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
