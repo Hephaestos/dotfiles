3f6a9dcc3c0eb5d50e2e58fd7b717b3a801b6155
Hephaestos <thedanielfloyd@protonmail.com>
2022-02-25 12:56:32 -0500

    set up ayu and nord posh themes

--- Profile.ps1
+++ Profile.ps1
@@ -1,14 +1,10 @@
-oh-my-posh --init --shell pwsh --config $HOME/.oh-my-posh-themes/night-owl-custom.omp.json | Invoke-Expression
+$POSH_THEME_DIR="$HOME/.oh-my-posh-themes"
+$POSH_THEME="ayu-owl.omp.json"
+oh-my-posh --init --shell pwsh --config "$POSH_THEME_DIR/$POSH_THEME" | Invoke-Expression
 Enable-PoshTransientPrompt
 Set-Alias -Name sudo -Value gsudo
 Set-Alias -Name vi -Value nvim
-Function llfunc {Get-ChildItem -Exclude .*}
-Set-Alias -Name ll -Value llfunc
-Set-Alias -Name l -Value llfunc
-Set-Alias -Name la -Value Get-ChildItem
-Remove-Alias -Name ls
-Function lsfunc {Get-ChildItem -Name -Exclude .*}
-Set-Alias -Name ls -Value lsfunc
+Set-Alias -Name l -Value ls
 Set-Alias -Name beep -Value $HOME\Documents\WindowsPowerShell\Scripts\beep.ps1
 Import-Module $HOME\Documents\WindowsPowerShell\Scripts\quickeos.ps1
 clear
